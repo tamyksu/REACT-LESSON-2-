336c07ce10312ea90fd0635fd4a6803340f6a915
Hackeru <Hackeru@Hackeru2.local>
2023-01-24 19:16:31 +0200

    leson 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name:{ nameF:'Tamy!', lastName: 'Fink'},
+      company:'c',
+    };
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          Hi {this.state.name.nameF} {this.state.name.lastName}works in {this.state.company}
+          </p>
+         <button onClick={()=>{
+          this.setState(
+
+          () =>{
+            return {name:{nameF:'Tcamy!', lastName: 'Ficccnk'}};
+          },
+          () => {//callback function, after the normal function finished
+              console.log("ff")        
+          }
+          );
+          console.log("d",this.state);
+         }}> Change name</button>
+        </header>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
