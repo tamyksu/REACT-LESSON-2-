a5c096e17967afc09304337892464adecf1d7523
Tamy Fink <57362830+tamyksu@users.noreply.github.com>
2023-01-31 20:29:39 +0200

    Add files via upload
    SEARCH ON BOX

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import logo from './logo.svg';
 import './App.css';
 import {Component} from 'react';
@@ -7,7 +9,7 @@ class App extends Component {
     super();
     this.state = {
           monsters:[
-            {
+          /*  {
               name:"TON",
               id:1,
             },
@@ -22,26 +24,47 @@ class App extends Component {
             {
               name:"dcc",
               id:4,
-            },
-          ]
+            },*/
+          ],
+          searchString: "",
     };
 
   }
   componentDidMount(){
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => 
-    console.log(res.json())
-    .then((users)= console.log(users))
-    );
+    .then((res) => res.json())
+    .then((users) => {
+      this.setState(() => {
+        return {monsters:users};//new object to state- update
+      },
+     ()=>{
+       console.log(this.state)//state status- display
+     });
+    });
+  }
+  onSearchChange = (event) =>{
+    const searchString = event.target.value.toLocaleLowerCase()
+    this.setState( ()=>{
+      return { searchString: searchString};
+     })
   }
   render(){
+    const {monsters,searchString} = this.state;//it's allow us to write without this.state
+    const filterList = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchString);
+   });
     return (
       <div  className="App" >
+      <input className='search-box'
+       type="search"
+        placeholder='search name'
+        onChange={this.onSearchChange}/>
         {
-      this.state.monsters.map((monster)=>{
+        filterList.map((monster)=>{
         return (
         <div key={monster.id}>
-        <h1 align = 'left' >{monster.name}</h1>
+        <h1 align = 'center' >{monster.name}</h1>
+        <p> {monster.email}</p>
        </div>
         );
       })
